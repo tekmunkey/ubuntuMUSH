009f8fa10e958222ad4aafa7083303387e34b6cc
tekmunkey <gitid@tekmunkey.com>
2017-12-25 09:40:11 -0700

    Fighting with tar over when and how to create a file so its permissions can be set appropriately.

--- doBackups.sh
+++ doBackups.sh
@@ -34,11 +34,6 @@ if [[ ! -d "${backupDirectory}" ]]; then
     chmod 700 "${backupDirectory}"
 fi
 
-if [[ ! -f "${compilationFile}" ]]; then
-    tar -cf "${compilationFile}"
-    chmod 700 "${compilationFile}"
-fi
-
 #
 # It's a good idea to back up your .ssh directory.  If you store this script at ~/ (your home)
 # then you can just uncomment the next backupTargets line and it will automatically add 
@@ -73,3 +68,9 @@ for bt in "${backupTargets[@]}"; do
 done
 
 tar -uf "${compilationFile}" "${backupDirectory}"
+#
+# There once was a block of code that created an empty tar if the compilation file didn't exist, but tar errored with the message: "cowardly 
+# refusing to create empty tar" all the time, and we couldn't set the file permission adequately.  So now we just have to set the stupid 
+# permission every stinking time we make a compilation file, otherwise we can't ever successfully set the permission
+#
+chmod 600 "${compilationFile}"
